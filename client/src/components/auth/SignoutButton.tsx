42598d3b1ebc17fab0261c36633670c5f85853c7
Connor Hay <cwhay888@gmail.com>
2024-06-12 12:16:31 -0400

    refactored authetication components

---
+++ client/src/components/auth/SignoutButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import api from '../../services/api';
+import { useNavigate } from 'react-router-dom';
+
+const SignoutButton: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleSignout = async () => {
+        try {
+            await api.post('/signout');
+            navigate('/signin');
+        } catch (error) {
+            console.error('Signout failed', error);
+        }
+    };
+
+    return (
+        <Button color="inherit" onClick={handleSignout} sx={{ mx:1 }}>
+            Signout
+        </Button>
+    );
+};
+
+export default SignoutButton;

4a9a4194af86d9b1dfade60f56d6f61021d87211
Connor Hay <cwhay888@gmail.com>
2024-06-16 00:27:19 -0400

    revised default data structure functions

--- client/src/types/activity.ts
+++ client/src/types/activity.ts
@@ -39,3 +39,28 @@ export function calculateActivityMetrics(activity: Omit<Activity, 'duration' | '
       loads,
   };
 }
+
+/**
+ * Generates a default activity with initialized values.
+ * Useful for creating new activity instances where no initial data is provided.
+ * Ensures that each field in an Activity has a safe initial value.
+ * @returns {Activity} A new activity object with default values.
+ */
+export const defaultNewActivity = (): Activity => ({
+  id:  -Number(DateTime.now().toISO()),            // A temporary ID that cannot conflict with database-assigned IDs
+  name: '',                             // Default empty name
+  startTime: DateTime.now().toISOTime(),    // Default start time in ISO format
+  endTime: DateTime.now().toISOTime(),      // Default end time in ISO format
+  notes: '',                            // Default empty notes
+  duration: 0,                          // Default duration of 0 minutes
+  intensities: {
+    fingers: 0,                         // Default intensity for fingers
+    upperBody: 0,                       // Default intensity for the upper body
+    lowerBody: 0,                       // Default intensity for the lower body
+  },
+  loads: {
+    fingers: 0,                         // Default load for fingers
+    upperBody: 0,                       // Default load for the upper body
+    lowerBody: 0,                       // Default load for the lower body
+  }
+});
--- client/src/types/session.ts
+++ client/src/types/session.ts
@@ -1,11 +1,11 @@
 import { DateTime } from 'luxon';
-import { Activity } from './activity';
+import { Activity, defaultNewActivity } from './activity';
 import { BodyPartMetrics } from './bodyPartMetrics';
 
 export interface Session {
     id: number;
-    completedAt:string; // ISO format
-    name: string;
+    date: string; // ISO format
+    name?: string;
     notes?: string;
     duration: number; // hours
     activities: Activity[];
@@ -19,15 +19,15 @@ export interface Session {
  * @returns {Session} A new session object with default values.
  */
 export const defaultNewSession = (): Session => ({
-    id: -new Date().getTime(),                 // A temporary ID that cannot conflict with database given IDs
+    id: -Number(DateTime.now().toISO()),                 // A temporary ID that cannot conflict with database given IDs
     date: DateTime.now().toISO(),              // Current date and time in ISO format
     name: '',                                  // Default empty name
     notes: '',                                 // Default empty notes
     duration: 0,                               // Default duration of zero
-    activities: [],                            // Empty activities array
+    activities: [defaultNewActivity()],                            // Empty activities array
     loads: {
       fingers: 0,                              // Default load for fingers
       upperBody: 0,                            // Default load for the upper body
       lowerBody: 0,                            // Default load for the lower body
     }
-  });
+});

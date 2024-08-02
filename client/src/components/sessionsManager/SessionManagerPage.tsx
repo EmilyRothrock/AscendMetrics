1d03c1d1b8fb928047a39aa372139cc183b24ce3
Connor Hay <cwhay888@gmail.com>
2024-08-02 10:53:33 -0400

    renamed diretories adn set up placeholder for SessionManagerPage

---
+++ client/src/components/sessionsManager/SessionManagerPage.tsx
@@ -0,0 +1,9 @@
+const SessionsManagerPage = () => {
+    // display sessions (pagination?)
+    // say what range you have
+    // prompt fetching of more sessions
+    // be able to open session editor
+    return <h1>This is the Sessions Manager Page</h1>
+}
+
+export default SessionsManagerPage;

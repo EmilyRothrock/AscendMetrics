44ce53408e4637c4389698b171a7819f994f3e21
Connor Hay <cwhay888@gmail.com>
2024-08-03 21:11:37 -0400

    responsified LoadBarChart, learned importance of setting wrapper div styling to 100% for width and height

--- client/src/components/charts/LoadBarChart.tsx
+++ client/src/components/charts/LoadBarChart.tsx
@@ -1,34 +1,40 @@
-import React, { useEffect, useRef } from 'react';
+import React, { MutableRefObject, RefObject, useEffect, useRef } from 'react';
 import { BodyPartMetrics } from '../../types';
 import { axisBottom, axisLeft, max, scaleBand, scaleLinear, select } from 'd3';
+import { useResizeObserver } from '../hooks/useResizeObserver';
 
-const LoadBarChart: React.FC<{ data: BodyPartMetrics; }> = ({ data }) => {
-  const ref = useRef();
+const LoadBarChart: React.FC<{ data: BodyPartMetrics }> = ({ data }) => {
+  const chartRef = useRef() as RefObject<SVGSVGElement>;
+  const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const dimensions = useResizeObserver(wrapperRef, { width: 400, height: 200 });
+
 
   const metricsArray = Object.entries(data).map(([part, load]) => ({ part, load }));
-  const colors = { fingers:"#2E96FF", upperBody:"#B800D8", lowerBody:"#02B2AF"};
-  const labels = { fingers:"Fingers", upperBody:"Upper Body", lowerBody:"Lower Body"}
 
   useEffect(() => {
-    const barChart = select(ref.current)
+    if (!chartRef.current || !dimensions) return;
+
+    const colors = { fingers:"#2E96FF", upperBody:"#B800D8", lowerBody:"#02B2AF"};
+    const labels = { fingers:"Fingers", upperBody:"Upper Body", lowerBody:"Lower Body"};
+
+    const barChart = select(chartRef.current)
       .style("overflow", "visible");
 
-    const xScale = scaleLinear()
-      .domain([0, max(metricsArray, d => d.load) as number])
-      .range([0, 150]);
+      const xScale = scaleLinear()
+      .domain([0, max(metricsArray, d => d.load) || 0])
+      .range([0, dimensions.width]);
     const xAxis = axisBottom(xScale)
       .ticks(3);
     barChart
       .select(".x-axis")
       .call(xAxis)            
-      .style("transform", "translateY(150px)");
+      .style("transform", `translateY(${dimensions.height}px)`);
 
     const yScale = scaleBand()
-      .range([0, 150])
       .domain(metricsArray.map(d => d.part))
+      .range([0, dimensions.height])
       .padding(0.3);
     const yAxis = axisLeft(yScale)
-      .ticks(0)
       .tickFormat("");
     barChart
       .select(".y-axis")
@@ -39,33 +45,32 @@ const LoadBarChart: React.FC<{ data: BodyPartMetrics; }> = ({ data }) => {
     barChart
       .selectAll(".bar")
       .data(metricsArray)
-      .enter()
-      .append("rect")
+      .join("rect")
       .attr("class", "bar")
       .attr("y", d => yScale(d.part)!)
       .attr("width", d => xScale(d.load))
       .attr("height", yScale.bandwidth())
-      .attr("fill", (d) => colors[d.part]);
+      .attr("fill", d => colors[d.part]);
     
     barChart
       .selectAll(".label")
       .data(metricsArray)
-      .enter()
-      .append("text")
+      .join("text")
       .attr("class", "label")
       .attr("y", d => yScale(d.part)! + yScale.bandwidth() / 2)
       .attr("x", 5) // slight offset from the start of the bar
       .attr("dy", "0.35em") // vertically center
-      .attr("text-anchor", "start")
       .style("font-size", "12px")
       .text(d => `${labels[d.part]}: ${d.load.toFixed(2)}`);
-  });
+  }, [dimensions, metricsArray]);
 
   return (
-    <svg ref={ref} width={"100%"} height={"100%"}>
-      <g className="x-axis"/>
-      <g className="y-axis"/>
-    </svg>
+    <div ref={wrapperRef} style={{ width:"100%", height:"100%" }}>
+      <svg ref={chartRef}>
+        <g className="x-axis"/>
+        <g className="y-axis"/>
+      </svg>
+    </div>
   );
 };
 

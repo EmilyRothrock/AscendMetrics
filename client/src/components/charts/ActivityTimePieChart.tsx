e2628438e3cb221a4b548bcf40217dda9ba2483d
Connor Hay <cwhay888@gmail.com>
2024-08-03 20:44:14 -0400

    responsified Activity Time Pie Chart, improved cleanliness and typing

--- client/src/components/charts/ActivityTimePieChart.tsx
+++ client/src/components/charts/ActivityTimePieChart.tsx
@@ -1,99 +1,108 @@
-import React, { useEffect, useRef } from 'react';
+import React, { MutableRefObject, RefObject, useEffect, useMemo, useRef, useState } from 'react';
 import { Activity } from '../../types';
 import { select, pie, arc, PieArcDatum, interpolateRainbow } from 'd3';
+import { useResizeObserver } from '../hooks/useResizeObserver';
 
-const ActivityTimePieChart: React.FC<{ activities: Activity[]; }> = ({ activities }) => {
-    const ref = useRef();
+interface ActivityDuration {
+    activity: string;
+    totalDuration: number;
+}
 
-    const data: { activity: string, totalDuration: number}[] = activities.reduce((acc, activity) => {
-        const existing = acc.find(a => a.activity === activity.name);
-        if (existing) {
+const ActivityTimePieChart: React.FC<{ activities: Activity[] }> = ({ activities }) => {
+    const chartRef = useRef() as RefObject<SVGSVGElement>;
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const dimensions = useResizeObserver(wrapperRef, { width: 200, height: 200 });
+    const [selectedActivity, setSelectedActivity] = useState<ActivityDuration | null>(null);
+
+    const data = useMemo(() => {
+        return activities.reduce((acc: ActivityDuration[], activity) => {
+          const existing = acc.find(a => a.activity === activity.name);
+          if (existing) {
             existing.totalDuration += activity.duration;
-        } else {
+          } else {
             acc.push({ activity: activity.name, totalDuration: activity.duration });
-        }
-        return acc;
-    }, [] as { activity: string, totalDuration: number }[]);
+          }
+          return acc;
+        }, [] as ActivityDuration[]);
+    }, [activities]);
 
+    // Basic Pie Structure
     useEffect(() => {
-        const radius = 80;
+        if (!chartRef.current) return;
+        const radius = Math.min(dimensions.height, dimensions.width)/2;
 
-        const pieChart = select(ref.current)
-            .selectAll('.pie')
-            .attr('transform', `translate(90, 90)`)
+        const svg = select(chartRef.current)
             .style("overflow", "visible");
 
-        // Pie generator - defines how value is handled
-        const myPie = pie<{ activity: string, totalDuration: number }>()
+        const pieChart = svg
+            .join('g')
+            .attr('class', 'pie-chart')
+            .attr('transform', `translate(${dimensions.width / 2}, ${dimensions.height / 2})`);
+
+        const myPie = pie<ActivityDuration>()
             .value(d => d.totalDuration);
 
-        // Arc generator - creates full objects with start and end angles
         const myArc = arc<PieArcDatum<{ activity: string, totalDuration: number }>>()
             .innerRadius(radius * 0.67)
             .outerRadius(radius);
 
-        // Execute generation
-        const arcs = pieChart
-            .selectAll('.arc')
+        const myArcs = pieChart.selectAll('.arc')
             .data(myPie(data))
-            .join('g')
+            .enter()
+            .append('g')
             .attr('class', 'arc');
 
-        arcs
+        myArcs
             .append('path')
             .attr('d', myArc)
-            .attr('fill', (d) => interpolateRainbow(Math.random()))
-            .on('mouseover', function (event, d) {
-                updateCentralText(d.data);
-            })
-            .on('mouseout', function () {
-                updateCentralText(null);
-            })
-            .on('click', function (event, d) {
-                updateCentralText(d.data);
-            });
+            .attr('fill', d => interpolateRainbow(Math.random()))
+            .on('mouseover', (_, d) => setSelectedActivity(d.data))
+            .on('mouseout', () => setSelectedActivity(null))
+            .on('click', (_, d) => setSelectedActivity(d.data));
 
-        const centralText = pieChart.selectAll(".center-text");
-
-        const updateCentralText = (data: { activity: string, totalDuration: number } | null) => {
-            centralText.selectAll("tspan").remove();
-            if (data) {            
-                const lines = [`${data.activity}:`, `${data.totalDuration.toFixed(0)} min.`];
-                const lineHeight = 1.2; // Line height in em units
-                const initialY = -((lines.length - 1) * lineHeight) / 2;
-                lines.forEach((line, i) => {
-                    centralText.append("tspan")
-                        .attr("x", 0)
-                        .attr("dy", i === 0 ? `${initialY}em` : `${lineHeight}em`)
-                        .text(line);
-                });
-            } else {
-                centralText.append("tspan")
-                    .attr("x", 0)
-                    .attr("dy", "-0.6em")
-                    .style("fill", "grey")
-                    .style("font-style", "italic")
-                    .text("hover/click");
-                centralText.append("tspan")
-                    .attr("x", 0)
-                    .attr("dy", "1.2em")
-                    .style("fill", "grey")
-                    .style("font-style", "italic")
-                    .text("for details");
-            }
-        }
-
-        updateCentralText(null); // initial rendering
-    });
+        svg.append("text")
+            .attr("class", "center-text")
+            .attr("text-anchor", "middle")
+            .style("font-size", "14px");
+    }, [data, dimensions]);
     
+    // For central text updates
+    useEffect(() => {
+        const svg = select(chartRef.current);
+        const centralText = svg.select(".center-text");
+        centralText.selectAll("tspan").remove(); // Clear previous texts
+    
+        centralText.selectAll("tspan").remove();
+        if (selectedActivity) {            
+            const lines = [`${selectedActivity.activity}:`, `${selectedActivity.totalDuration.toFixed(0)} min.`];
+            const lineHeight = 1.2; // Line height in em units
+            const initialY = -((lines.length - 1) * lineHeight) / 2;
+            lines.forEach((line, i) => {
+                centralText.append("tspan")
+                    .attr("x", 0)
+                    .attr("dy", i === 0 ? `${initialY}em` : `${lineHeight}em`)
+                    .text(line);
+            });
+        } else {
+            centralText.append("tspan")
+                .attr("x", 0)
+                .attr("dy", "-0.6em")
+                .style("fill", "grey")
+                .style("font-style", "italic")
+                .text("hover/click");
+            centralText.append("tspan")
+                .attr("x", 0)
+                .attr("dy", "1.2em")
+                .style("fill", "grey")
+                .style("font-style", "italic")
+                .text("for details");
+        }
+    }, [selectedActivity]);
+
     return (
-        <svg ref={ref} width={"100%"} height={"100%"}>
-            <g className="pie">
-                <text className="center-text" textAnchor="middle" style={{ fontSize:"14px" }}>
-                    <tspan>testing</tspan>
-                </text>
-            </g>
-        </svg>
+        <div ref={wrapperRef}>
+            <svg ref={chartRef}/>
+        </div>
     );
 }
 
